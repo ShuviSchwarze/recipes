bfc3d083606f87798cf280be54e806b3f575e264
ShuviSchwarze <shuvischwarze@gmail.com>
2023-06-07 14:05:25 +0700

    Collected Dockerfiles

--- api/api/models.py
+++ api/api/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.conf import settings
 
 from . import constants
 
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     avatar = models.ImageField(max_length=255, blank=True, null=True)
 
     def json_default():
--- api/api/views.py
+++ api/api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from . import models
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+from django.views import View
 
-# Create your views here.
+import json
+from functools import wraps
+
+
+class RegistrationForm(View):
+
+    def post(self, request) -> JsonResponse:
+        #username = request.POST.get("username")
+        #email = request.POST.get("email")
+        #password = request.POST.get("password")
+        #
+        #if validate_user(username, email, password) :       
+        #    new_user = models.User.create(
+        #        username=username,
+        #        email=email,
+        #        password=password,
+        #    )
+        #
+        #json = presenter.createJSon(new_user)
+        
+        
+        return #JsonResponse()
